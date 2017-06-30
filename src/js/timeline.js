05bac3a92c2b673e678d02993b023ee1a5a1dc05
xccgra <xccgra@163.com>
2017-06-30 15:03:16 +0800

    timeline图片

---
+++ src/js/timeline.js
@@ -0,0 +1,13 @@
+
+    	function show_img(x="img_1")//显示图片函数，鼠标移到链接触发
+    	{
+        	document.getElementById(x).style.display = "block";
+        }
+
+    	function hide_img(x="img_1")//隐藏图片函数，鼠标离开链接触发
+    	{
+        	document.getElementById(x).style.display = "none";
+        }
+
+
+
