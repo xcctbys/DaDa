7dc9a69df8eb66de223f701c41f138a4d05cb47b
witchwen7 <witchwen@163.com>
2017-07-06 20:39:53 +0800

    totally changed the structure

---
+++ src/js/googlegg.js
@@ -0,0 +1,7 @@
+document.writeln("<div style=\"margin:auto;width:830px;\">");
+document.writeln("<script async src=\"/js/adsbygoogle.js\"><\/script>");
+document.writeln("<ins class=\"adsbygoogle\" style=\"display:inline-block;width:830px;height:120px\" data-ad-client=\"ca-pub-4078329886972765\" data-ad-slot=\"4189420132\"><\/ins>");
+document.writeln("<script>");
+document.writeln("(adsbygoogle = window.adsbygoogle || []).push({});");
+document.writeln("<\/script>");
+document.writeln("<\/div>");
