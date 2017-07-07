aac78902b9da8d43f7720e098386d493f9983984
xccgra <xccgra@163.com>
2017-07-07 17:50:10 +0800

    timeline schedule

--- src/js/timeline.js
+++ src/js/timeline.js
@@ -1,10 +1,10 @@
 
-    	function show_img(x="img_1")//显示图片函数，鼠标移到链接触发
+    	function show_img(x="schedule1")//显示图片函数，鼠标移到链接触发
     	{
         	document.getElementById(x).style.display = "block";
         }
 
-    	function hide_img(x="img_1")//隐藏图片函数，鼠标离开链接触发
+    	function hide_img(x="schedule1")//隐藏图片函数，鼠标离开链接触发
     	{
         	document.getElementById(x).style.display = "none";
         }
