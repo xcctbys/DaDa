029e0f21f3da7b2fc66ca460668e114c513f0349
xccgra <xccgra@163.com>
2017-07-09 17:30:08 +0800

    conflict

--- src/js/timeline.js
+++ src/js/timeline.js
@@ -1,22 +1,33 @@
-
-jQuery(document).ready(function($)
-{
+function show_img(x="schedule1")//显示图片函数，鼠标移到链接触发		
+	{
+	       	document.getElementById(x).style.display = "block";		
 	
-	$("#sp1, #sp2, #sp3, #sp4").mouseover(function(){
-		var schedule = "#" + $(this).html();
-		// console.log(schedule);
-		$(schedule).show();
-		// console.log("mouseover");
-	});
-	$("#sp1, #sp2, #sp3, #sp4").mouseout(function(){
-		var schedule = "#" + $(this).html();
-		// console.log(schedule);
-		$(schedule).hide();
-		// console.log("mouseout");
-	});
-		
-});
+	}
+ 
+function hide_img(x="schedule1")//隐藏图片函数，鼠标离开链接触发		
+	{
+	     	document.getElementById(x).style.display = "none";		
+	}
 
 
 
+
+//
+//jQuery(document).ready(function($)
+//{
+//	
+//	$("#sp1, #sp2, #sp3, #sp4").mouseover(function(){
+//		var schedule = "#" + $(this).html();
+//		// console.log(schedule);
+//		$(schedule).show();
+//		// console.log("mouseover");
+//	});
+//	$("#sp1, #sp2, #sp3, #sp4").mouseout(function(){
+//		var schedule = "#" + $(this).html();
+//		// console.log(schedule);
+//		$(schedule).hide();
+//		// console.log("mouseout");
+//	});
+//		
+//});
 
