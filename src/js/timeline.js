252c07952405da4f9b69c0297de7a1125562954f
xccgra <xccgra@163.com>
2017-07-09 11:56:09 +0800

    调整banner  调整cloud layer 百分比 schedule  手机端适配

--- src/js/timeline.js
+++ src/js/timeline.js
@@ -1,13 +1,22 @@
 
-    	function show_img(x="schedule1")//显示图片函数，鼠标移到链接触发
-    	{
-        	document.getElementById(x).style.display = "block";
-        }
+jQuery(document).ready(function($)
+{
+	
+	$("#sp1, #sp2, #sp3, #sp4").mouseover(function(){
+		var schedule = "#" + $(this).html();
+		// console.log(schedule);
+		$(schedule).show();
+		// console.log("mouseover");
+	});
+	$("#sp1, #sp2, #sp3, #sp4").mouseout(function(){
+		var schedule = "#" + $(this).html();
+		// console.log(schedule);
+		$(schedule).hide();
+		// console.log("mouseout");
+	});
+		
+});
 
-    	function hide_img(x="schedule1")//隐藏图片函数，鼠标离开链接触发
-    	{
-        	document.getElementById(x).style.display = "none";
-        }
 
 
 
