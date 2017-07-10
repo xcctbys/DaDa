9482c7d1a15ded992ee9baba1ac089c034a3b190
xccgra <xccgra@163.com>
2017-07-10 22:17:18 +0800

    click

--- src/js/timeline.js
+++ src/js/timeline.js
@@ -8,6 +8,36 @@ function hide_img(x="schedule1")//隐藏图片函数，鼠标离开链接触发
 	{
 	     	document.getElementById(x).style.display = "none";		
 	}
+function show_assay(x="assay6")
+
+{
+	document.getElementById("assay6").style.display = "none";
+	document.getElementById("assay7").style.display = "none";
+	document.getElementById("assay8").style.display = "none";
+	document.getElementById("assay9").style.display = "none";
+	document.getElementById("assay10").style.display = "none";
+	document.getElementById("assay11").style.display = "none";
+	document.getElementById(x).style.display = "block";
+
+
+}//显示图片函数，鼠标移到链接触发	
+
+function show_schedule(x="schedule1")
+
+{
+	document.getElementById("schedule1").style.display = "none";
+	document.getElementById("schedule2").style.display = "none";
+	document.getElementById("schedule3").style.display = "none";
+	document.getElementById("schedule4").style.display = "none";
+	document.getElementById(x).style.display = "block";
+
+
+}//显示图片函数，鼠标移到链接触发	
+
+
+
+
+
 
 
 
